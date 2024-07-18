fd3961bceea52c8408ef00c949b79501b48c9f8c
Artur Aroca <arthurandres30@gmail.com>
2024-07-18 11:30:56 -0500

    Desarrollo Endpoint para abrir ruleta

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-// Importar las librerías necesarias
+// se Importa las librerías necesarias
 const express = require('express');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
+const morgan = require("morgan");
 
-// Crear una nueva instancia de Express
+//se Crea una nueva instancia de Express
 const app = express();
 
-// Configurar body-parser para manejar solicitudes JSON
+// Configuración del body-parser para manejar solicitudes JSON
 app.use(bodyParser.json());
+
+//middlewares
+app.use(morgan("dev"));
 
 // Array para almacenar las ruletas creadas
 let ruletas = [];
@@ -21,6 +25,7 @@ app.get('/', (req, res) => {
 app.post('/crearRuleta', (req, res) => {
     const nuevaRuleta = {
         id: uuidv4(), // Genera un id para la nueva ruleta
+        estado: 'cerrada', //Inicialmente, la ruleta esta cerrada
         ...req.body
     };
 
@@ -30,12 +35,36 @@ app.post('/crearRuleta', (req, res) => {
     res.status(201).json({ id: nuevaRuleta.id });
 });
 
+//Endpoint para abrir una ruleta
+app.post('/abrirRuleta', (req, res) => {
+    try{
+        const { id } = req.body;
+        //Verificacion si el ID esta presente, si no esta presente se devuelte una respuesta con codigo HTTP 400
+        if (!id) {
+            return res.status(400).json({ estado: 'denegado', mensaje: 'ID es requerido'});
+        }
+        
+        const ruleta = ruleta.find(r = r.id === id);
+        if (!ruleta) {
+            return res.status(404).json({estado: 'denegado', mensaje: 'Ruleta no encontrada'});
+        }
+
+        if (ruleta.estadoi === 'abierta') {
+            return res.status(400).json({estado: 'denegado', mensaje: 'La ruleta ya esta abierta'});
+        }
+
+        ruleta.estado = 'estado';
+        res.status(200),json({estado: 'Exitoso'});
+    } catch (error) {
+        res.status(500).json({estado: 'denegado', mensaje: 'Error del servidor'});
+    }
+})
 // Manejador de rutas no encontradas (404)
 app.use((req, res) => {
     res.status(404).send('Ruta no encontrada');
 });
 
-// Configurar el puerto en el que va a escuchar el servidor
+// Configura el puerto en el que va a escuchar el servidor
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor Express escuchando en el puerto ${PORT}`);
